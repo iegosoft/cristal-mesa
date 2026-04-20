f2f20eedb67ebbcfcce55ba3a8a6399fd7642995
IegoCosta <iegosergio1320@gmail.com>
2026-04-20 17:08:45 -0400

    remove restos de conflito site.ts

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -3,7 +3,6 @@
 
 export const WHATSAPP_NUMBER = "5592993960786";
 export const SITE_NAME = "Vieira Decor";
->>>>>>> 0737c188df8b3de7a66c7e46c1988699d0c0b2ce
 export const SITE_TAGLINE = "Mesa posta, louças e cristais para encantar";
 export const CONTACT_EMAIL = "contato@vieiradecor.com.br";
 export const INSTAGRAM = "@vieiradecor";
